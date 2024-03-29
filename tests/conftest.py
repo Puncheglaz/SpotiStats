8e5702311f8371a972bc6fa5bb948d236db268cb
TheTedLab <71270225+TheTedLab@users.noreply.github.com>
2024-03-29 21:22:33 +0300

    Update confest.py
    Remove unnecessary fixture for headers taking

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,13 +2,6 @@ import json
 
 import pytest
 
-from src.aggregator.auth_credentials import access_token, token_type
-
-
-@pytest.fixture()
-def get_headers():
-    return {'Authorization': f'{token_type}  {access_token}'}
-
 
 @pytest.fixture()
 def get_artist_json_data():
