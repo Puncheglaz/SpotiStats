7a57d8f6f09bd8e466d04a29ca3bf19221a4c5f0
TheTedLab <71270225+TheTedLab@users.noreply.github.com>
2024-05-05 23:23:59 +0300

    Add logging to modules
    Add logging via loguru with config yaml file
    Change prints to logger calls
    Add app.py main application module for start and logging

--- src/aggregator/artist_aggregate.py
+++ src/aggregator/artist_aggregate.py
@@ -2,6 +2,7 @@
 import json
 
 import requests
+from loguru import logger
 
 
 def get_related_artists(artists_ids, headers, stage=0):
@@ -15,7 +16,7 @@ def get_related_artists(artists_ids, headers, stage=0):
     process_num = 0
     for artist_id in artists_ids:
         process_num += 1
-        print(f'Collect {process_num} of {total_artists}')
+        logger.debug(f'Collect {process_num} of {total_artists}')
         response = requests.get(
             f'https://api.spotify.com/v1/artists/{artist_id}/related-artists',
             headers=headers,
@@ -25,10 +26,10 @@ def get_related_artists(artists_ids, headers, stage=0):
         for artist in response.json().get('artists'):
             artists_ids_list.append(artist.get('id'))
 
-    print(f'==============STAGE={stage}=============')
-    print(f'       Total related artists: {len(artists_ids_list)}')
-    print(f'Total unique related artists: {len(set(artists_ids_list))}')
-    print('==================================')
+    logger.debug(f'==============STAGE={stage}=============')
+    logger.info(f'       Total related artists: {len(artists_ids_list)}')
+    logger.info(f'Total unique related artists: {len(set(artists_ids_list))}')
+    logger.debug('==================================')
 
     artists_ids_list = list(set(artists_ids_list))
 
@@ -37,6 +38,7 @@ def get_related_artists(artists_ids, headers, stage=0):
 
 def artist_aggregate_main(source_file_path, id_file_path, headers):
     """Main function for data aggregation."""
+    logger.info('Starting artist aggregation')
     with open(
             source_file_path,
             mode='r',
@@ -48,12 +50,14 @@ def artist_aggregate_main(source_file_path, id_file_path, headers):
     for artist in artists_data.get('artists'):
         followed_artists_ids.append(artist.get('uri').split(':')[2])
 
+    logger.info(f"Artist aggregation stage one")
     artists_ids_list_stage_one = get_related_artists(
         followed_artists_ids,
         headers=headers,
         stage=1
     )
 
+    logger.info(f"Artist aggregation stage two")
     artists_ids_list = get_related_artists(
         artists_ids_list_stage_one,
         headers=headers,
@@ -72,7 +76,7 @@ def artist_aggregate_main(source_file_path, id_file_path, headers):
             encoding='utf-8'
     ) as file:
         file.write(json_string)
-
+    logger.info(f"Artist aggregation complete")
 
 # if __name__ == '__main__':
 #     artist_aggregate_main(
